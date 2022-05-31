68a108ae6ad3f5b829a4f73c2142fdb01d7061ba
eddgalan <edd.galan@hotmail.com>
2022-05-31 13:24:41 -0500

    Add Button Component - Se creó el componente Button - Se hizo el front de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './img/AlphaTeamDev-sm.png';
+import ButtonCalc from './components/buttons/button.jsx';
 
 function App() {
   return (
@@ -8,10 +9,30 @@ function App() {
         <img className="logo" src={ logo } alt="Logo AlphaTeamDev" />
       </div>
       <div className="calculator-container">
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+        <div className="row">
+          <ButtonCalc> 1 </ButtonCalc>
+          <ButtonCalc> 2 </ButtonCalc>
+          <ButtonCalc> 3 </ButtonCalc>
+          <ButtonCalc> + </ButtonCalc>
+        </div>
+        <div className="row">
+          <ButtonCalc> 4 </ButtonCalc>
+          <ButtonCalc> 5 </ButtonCalc>
+          <ButtonCalc> 6 </ButtonCalc>
+          <ButtonCalc> - </ButtonCalc>
+        </div>
+        <div className="row">
+          <ButtonCalc> 7 </ButtonCalc>
+          <ButtonCalc> 8 </ButtonCalc>
+          <ButtonCalc> 9 </ButtonCalc>
+          <ButtonCalc> * </ButtonCalc>
+        </div>
+        <div className="row">
+          <ButtonCalc> = </ButtonCalc>
+          <ButtonCalc> 0 </ButtonCalc>
+          <ButtonCalc> . </ButtonCalc>
+          <ButtonCalc> / </ButtonCalc> 
+        </div>
         <div className="row"></div>
       </div>
     </div>
---
+++ src/components/buttons/button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './button.css';
+
+function ButtonCalc(props) {
+  const isOperator = value => {
+    return isNaN(value) && (value != '.') & (value != '=');
+  };
+
+  return (
+    <div className={ `button-container ${ isOperator(props.children) ? 'operador' : '' }`.trimEnd() }>
+      { props.children }
+    </div>
+  );
+}
+
+export default ButtonCalc;
