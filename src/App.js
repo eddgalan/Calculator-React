ccd8c8e86856e57e39f4d55c780bcfdcc5b82671
eddgalan <edd.galan@hotmail.com>
2022-05-31 00:11:35 -0500

    Style and images - Se agregó la estructura, estilo e imágenes para la aplicación

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import logo from './img/AlphaTeamDev-sm.png';
 
 function App() {
   return (
     <div className="App">
-      Hola
+      <div className="logo-container">
+        <img className="logo" src={ logo } alt="Logo AlphaTeamDev" />
+      </div>
+      <div className="calculator-container">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
     </div>
   );
 }
